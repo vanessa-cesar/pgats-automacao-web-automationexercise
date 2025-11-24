c41367fd8d3a1d3d0cbbbcda3085b98a7c2594a4
Vanessa Cesar de Souza <vancesar.souza@gmail.com>
2025-11-24 17:12:24 -0300

    Fix: Corrigindo sintaxe YAML do workflow Cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  retries: {
+     openMode: 0,
+     runMode: 1,
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-mochawesome-reporter/plugin')(on);
